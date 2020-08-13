8bfb387df59f1a4c850ff87af321d9e65fcf3832
Jim Tseng <jimtseng@Macbook-Pro.attlocal.net>
2020-08-13 15:59:47 -0700

    Updated Components and Added Dockerfile

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+//import { AutoContexts } from './contexts/AutoContexts'
+import {  AutoProvider } from './contexts/AutoContexts'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import '../src/ui.css'
+import '../src/index.css'
 
-export default App;
+import firebase from './firebase'
+
+import Home from './components/Home'
+import Login from './components/Login'
+import SignUp from './components/SignUp'
+import Verifyemail from './components/Verifyemail'
+import Logout from './components/Logout'
+
+const App = React.memo((ckemailVerified) => {
+	const [loggedinuser, setLoggedinUser] = useState('')
+	const [emailVerified, setEmailVerified] = useState(false)
+	const [gotologin, setGotologin] = useState(false)
+
+//	const auto = useContext(AutoContexts)
+//	auto.somefunction()
+
+    useEffect( () => {
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) { 
+
+			console.log("Logged In User")
+			// eslint-disable-next-line
+			ckemailVerified = user.emailVerified
+			setEmailVerified(user.emailVerified)
+			setLoggedinUser(user.email)
+			setGotologin(false)
+
+			console.log("APP user", user)
+			console.log("APP useremail", user.email)
+			console.log("APP loggedin", loggedinuser)
+			console.log("APP Length", loggedinuser.length)
+			console.log("APP verified 1", emailVerified)
+			console.log("APP verified 2", user.emailVerified)
+			console.log("APP ckemailverified", ckemailVerified)
+
+            } else {
+
+			console.log("ckemailVerified", emailVerified)
+			setEmailVerified(false)
+			setLoggedinUser('')
+			setGotologin(true)
+			console.log("Not Logged In")
+			return <Redirect to='/login'  />
+
+			}
+		})
+
+    }, [])
+
+    return (
+        <AutoProvider>
+            <Router>
+                <Switch>
+                    <Route exact path="/signup" component={SignUp} />
+                    <Route exact path="/verifyemail" component={Verifyemail} />
+                    <Route exact path="/logout" component={Logout} />
+					<Route exact path="/login" component={Login} />
+
+					{ (loggedinuser.length>0 && !ckemailVerified) && <Route exact path="/" component={Verifyemail} /> }
+					{/* { (loggedinuser.length>0 && ckemailVerified) && <Route exact path="/" component={Home} /> } */}
+					{ (loggedinuser.length<=0 && !ckemailVerified) && <Route exact path="/" component={Login} /> }
+					{ (gotologin) ? <Route exact path="/" component={Login} /> : <Route exact path="/" component={Home} /> }
+					{/* { (loggedinuser.length<=0 && ckemailVerified) && <Route exact path="/" component={Test} /> } */}
+
+					{/* { (loggedinuser.length<=0) && <Route exact path="/" component={Login} /> }
+					{ (loggedinuser.length>0 && !emailVerified) && <Route exact path="/" component={Verifyemail} /> }
+					{ (loggedinuser.length>0 && emailVerified) && <Route exact path="/" component={Home} /> } */}
+
+                </Switch>  
+            </Router>
+        </AutoProvider>
+    )
+})
+
+export default App
